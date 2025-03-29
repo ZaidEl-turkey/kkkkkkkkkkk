42fc76dfa038d02061785adea7d8a5aaa61704c4
ZaidEl-turkey <zaidelturkey798@gmail.com>
2025-03-29 17:22:26 +0200

    changes

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ function addGoal() {
 function removeGoal(button) {
     button.parentElement.remove();
 }
+const resetBtn =document.getElementById("resetBtn");
+const increaseBtn =document.getElementById("increaseBtn");
+const countlabel =document.getElementById("countlabel");
+let count =0 ;
+
+increaseBtn.onclick= function(){
+    count++;
+    countlabel.textContent=count;
+
+}
+
+resetBtn.onclick= function(){
+    count=0;
+    countlabel.textContent=count;
+
+}
